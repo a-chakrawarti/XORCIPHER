7f211e8d5b83e3e2a77be991ba23f9d2e4ef83c4
Anupam Chakrawarti <chakrawarti.anupam@yahoo.com>
2018-07-30 15:46:36 +0000

    few minor changes

--- run.py
+++ run.py
@@ -19,35 +19,39 @@ def stringtobinary(xt):
         xt_binary.append(xt_bool)
     return xt_binary
 
+def xoring(xt_binary,key_binary):       # XOR of cipher/plain text with key during encryption/decryption
+    final_binary=[]
+    xt=[]
+    for i in range(0, len(xt_binary)):
+        xt_list=list()
+        for j in range(0, len(xt_binary[i])):
+            xt_element = xor(int(xt_binary[i][j]), int(key_binary[i][j]))
+            xt_element = str(xt_element)
+            xt_list.append(xt_element)
+        xt_list = ''.join(xt_list)
+        final_binary.append(xt_list)
+        del xt_list
+
+    for i in final_binary:
+        val = int(i, 2)
+        char = chr(val)
+        xt.append(char)
+        text = ''.join(xt)
+    return text
+
 def encrypt(plaintext):
     """Enter the message to be encrypted."""
     key_binary = list()
     pt_binary = list()
-    ciphert = []
     ciphertext = []
-    ct_binary = list()
     pt_len = len(plaintext)
 
-    key = ''.join(random.choice(string.ascii_lowercase) for n in range(pt_len))  # Generation of Key
+    key = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(pt_len))  # Generation of Key
 
     pt_binary = stringtobinary(plaintext)
     key_binary = stringtobinary(key)
-
-    for i in range(0, len(pt_binary)):
-        ct_list = list()
-        for j in range(0, len(pt_binary[i])):
-            ct_element = xor(int(pt_binary[i][j]), int(key_binary[i][j]))
-            ct_element = str(ct_element)
-            ct_list.append(ct_element)
-        ct_list = ''.join(ct_list)
-        ct_binary.append(ct_list)
-        del (ct_list)
-
-    for i in ct_binary:
-        val = int(i, 2)
-        char = chr(val)
-        ciphert.append(char)
-        ciphertext = ''.join(ciphert)
+    
+    ciphertext = xoring(pt_binary,key_binary)
 
     ciphertext = ciphertext.encode()
     ciphertext = base64.b64encode(ciphertext)
@@ -73,28 +77,13 @@ def decrypt(key, ciphertext):
     ct_binary = stringtobinary(ciphertext)
     key_binary = stringtobinary(key)
 
-
-    for i in range(0, len(ct_binary)):
-        pt_list = list()
-        for j in range(0, len(ct_binary[i])):
-            pt_element = xor(int(ct_binary[i][j]), int(key_binary[i][j]))
-            pt_element = str(pt_element)
-            pt_list.append(pt_element)
-        pt_list = ''.join(pt_list)
-        pt_binary.append(pt_list)
-        del pt_list
-
-    del ct_binary
-
-    for i in pt_binary:
-        val = int(i, 2)
-        char = chr(val)
-        plaint.append(char)
-        plaintext = ''.join(plaint)
+    plaintext = xoring(ct_binary,key_binary)
 
     print('Plaintext :', plaintext)
 
-script, method = argv
+
+
+method = argv[1]
 
 if method == '-encrypt':
     plaintext = input("Enter the message : ")
@@ -106,9 +95,9 @@ elif method == '-decrypt':
     decrypt(key, ciphertext)
 
 elif (method != '-encrypt') or (method != '-decrypt'):
-    help = '''
+    _help_ = '''
     > Enter valid argument :
     \t-encrypt : To encrypt plaintext.
     \t-decrypt : To decrypt ciphertext provided correct key.
     '''
-    print(help)
+    print(_help_)
